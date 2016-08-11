8c46c458e3f1262d12f005ddebe2ef34be392841
tamilhce <tamilhce@gmail.com>
2016-08-11 15:27:02 +0530

    ading my ownwq

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: "<h1>My First Angular 2 App<br>\
+    <form>\
+    <input type='text' name='FirstName' value='Mickey'><br> \
+    </form>\
+</h1>"
+ 
+
+               
 })
 export class AppComponent { }
